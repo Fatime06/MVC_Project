2977c740165a3879b1cf3e6cedac95a8fec19d50
Fatime06 <fatimafa@code.edu.az>
2025-02-13 04:08:04 +0400

    added first commit

---
+++ Data/JuanDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Juan_Mvc_Project.Data
+{
+    public class JuanDbContext : DbContext
+    {
+        public JuanDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+using Juan_Mvc_Project.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<JuanDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+var app = builder.Build();
+
+app.MapDefaultControllerRoute();
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.Run();
